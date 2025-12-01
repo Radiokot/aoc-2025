9436a1940f37deed937e66824a7949ff7711ce81
Oleg Koretsky <oleg@radiokot.com.ua>
2025-12-01 18:04:04 +0200

    Solve the 1st day

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,55 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var dial = 50
+        var password = 0
+        input
+            .filter(String::isNotEmpty)
+            .map(::getDirectionAndClicks)
+            .forEach { (direction, clicks) ->
+                repeat(clicks) {
+                    dial = (100 + dial + direction) % 100
+                }
+                if (dial == 0) {
+                    password++
+                }
+            }
+
+        return password
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var dial = 50
+        var password = 0
+        input
+            .filter(String::isNotEmpty)
+            .map(::getDirectionAndClicks)
+            .forEach { (direction, clicks) ->
+                repeat(clicks) {
+                    dial = (100 + dial + direction) % 100
+                    if (dial == 0) {
+                        password++
+                    }
+                }
+            }
+
+        return password
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+    val input =
+        readInput("Day01")
+//        readInput("Day01_test")
 
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
+}
+
+private fun getDirectionAndClicks(instruction: String): Pair<Int, Int> {
+    val direction =
+        if (instruction.take(1) == "L")
+            -1
+        else
+            1
+    val clicks = instruction.drop(1).toInt()
+
+    return direction to clicks
 }
